3ab14aab2ac1ed7f9a44b176aa8cc48294234e58
RajCutinha <rajath.cutinha@protonmail.com>
2022-09-09 23:55:35 +0200

    inital commit of config files

---
+++ Windows/Terminal/PS_Profile.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/1_shell.omp.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -HistorySavePath "$env:USERPROFILE\.config\history"
+Set-PSReadLineOption -HistoryNoDuplicates
